eddba96eb4d7cb48b9bda07b1a046cb3e72f134d
Abhishek Kumar <akumar074@gmail.com>
2025-06-13 14:12:33 +0530

    Fixing indenation

--- tools/csv-gen/csv-gen.go
+++ tools/csv-gen/csv-gen.go
@@ -325,12 +325,12 @@ func main() {
 			},
 		}
 
-        // update/create cluster roles and bindings / service account yaml files
-        createFile("deploy/role.yaml", role)
-        createFile("deploy/cluster_role.yaml", clusterRole)
-        createFile("deploy/cluster_role_binding.yaml", components.GetClusterRoleBinding(csv.OperatorName))
-        createFile("deploy/role_binding.yaml", components.GetRoleBinding(csv.OperatorName))
-        createFile("deploy/service_account.yaml", components.GetServiceAccount(csv.OperatorName))
+		// update/create cluster roles and bindings / service account yaml files
+		createFile("deploy/role.yaml", role)
+		createFile("deploy/cluster_role.yaml", clusterRole)
+		createFile("deploy/cluster_role_binding.yaml", components.GetClusterRoleBinding(csv.OperatorName))
+		createFile("deploy/role_binding.yaml", components.GetRoleBinding(csv.OperatorName))
+		createFile("deploy/service_account.yaml", components.GetServiceAccount(csv.OperatorName))
 
 		bundleDir := "deploy/olm-catalog/prod/" + *ver + "/"
 		if csv.Maturity == "dev" {
